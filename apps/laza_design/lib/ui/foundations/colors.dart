de8a2b40ee505f5ce83a8ef2543bdaac09b6252a
Sang Duong <sang.duong@asnet.com.vn>
2024-06-27 17:22:17 +0700

    Implement theme, color, typography of app

---
+++ apps/laza_design/lib/ui/foundations/colors.dart
@@ -0,0 +1,49 @@
+import 'dart:ui';
+import 'package:flutter/widgets.dart';
+export 'package:flutter/foundation.dart' show Brightness;
+
+class LSColors {
+  // White color
+  static const white = Color(0xFFFFFFFF);
+
+  // Black color
+  static const black = Color(0xFF000000);
+
+  // Primary color
+  static const primary50 = Color(0xFFE9EFFD);
+  static const primary100 = Color(0xFFA4D5F5);
+  static const primary200 = Color(0xFF8BC2F3);
+  static const primary300 = Color(0xFF72ADF0);
+  static const primary400 = Color(0xFF5894ED);
+  static const primary500 = Color(0xFF1DA1F2);
+  static const primary600 = Color(0xFF3D93F8);
+  static const primary700 = Color(0xFF4267B2);
+  static const primary800 = Color(0xFF15158D);
+  static const primary900 = Color(0xFF180D5F);
+
+  // Error color
+  static const error100 = Color(0xFFFEE4E2);
+  static const error200 = Color(0xFFFECDCA);
+  static const error300 = Color(0xFFDE8588);
+  static const error400 = Color(0xFFF97066);
+  static const error500 = Color(0xFFFF5757);
+  static const error600 = Color(0xFFEA4335);
+  static const error700 = Color(0xFFB32318);
+  static const error800 = Color(0xFF912018);
+  static const error900 = Color(0xFF7A271A);
+
+  // Grey color
+  static const grey50 = Color(0xFFEDEEF0);
+  static const grey100 = Color(0xFFCFD1D4);
+  static const grey200 = Color(0xFFB7BABF);
+  static const grey300 = Color(0xFF9FA3A9);
+  static const grey400 = Color(0xFF888C94);
+  static const grey500 = Color(0xFF8F959E);
+  static const grey600 = Color(0xFF585D69);
+  static const grey700 = Color(0xFF404653);
+  static const grey800 = Color(0xFF282F3E);
+  static const grey900 = Color(0xFF1D1E20);
+
+  // Other color
+  static const mostlyBlack = Color(0xFF1D1E20);
+}
---
+++ apps/laza_design/lib/ui/tokens/typography.dart
@@ -0,0 +1,7 @@
+import 'package:laza_design/core/gen_assets/fonts.gen.dart';
+
+class LSUiTypography {
+  LSUiTypography._();
+  // Fonts
+  static const String familyInter = FontFamily.inter;
+}
