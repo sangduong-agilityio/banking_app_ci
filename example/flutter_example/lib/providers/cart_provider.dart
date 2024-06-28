e29a6f538aa116f465545aa919d8c8f6097c0390
Sang Duong <sang.duong@asnet.com.vn>
2024-06-28 15:11:04 +0700

    Implement example using riverpod state management

---
+++ example/flutter_example/lib/providers/cart_provider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_example/models/product.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
+  // initial value
+  @override
+  Set<Product> build() {
+    return const {};
+  }
+
+  void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+  }
+
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
+      state = state.where((p) => p.id != product.id).toSet();
+    }
+  }
+}
+
+@riverpod
+int cartTotal(ref) {
+  final cartProducts = ref.watch(cartNotifierProvider);
+
+  int total = 0;
+
+  for (Product product in cartProducts) {
+    total += product.price;
+  }
+  return total;
+}
