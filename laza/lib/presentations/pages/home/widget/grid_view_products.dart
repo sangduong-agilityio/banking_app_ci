b840ee6c54a16f6bb070b34ba6be9f6fe486ec7c
Sang Duong <sang.duong@asnet.com.vn>
2024-07-23 17:05:43 +0700

    Implement process call API for Home screen

--- laza/lib/presentations/pages/home/widget/grid_view_products.dart
+++ laza/lib/presentations/pages/home/widget/grid_view_products.dart
@@ -1,34 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:laza/core/gen_assets/assets.gen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:laza/presentations/widgets/product_card.dart';
+import 'package:laza/providers/product_provider.dart';
 
-class GridViewProduct extends StatelessWidget {
+class GridViewProduct extends ConsumerWidget {
   const GridViewProduct({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: GridView.builder(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productsAsyncValue = ref.watch(productsProvider);
+
+    return productsAsyncValue.when(
+      data: (products) => GridView.builder(
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: 7 / 10,
+          childAspectRatio: 7 / 11,
           mainAxisSpacing: 15,
           crossAxisCount: 2,
           crossAxisSpacing: 15,
         ),
-        itemCount: 8,
+        itemCount: products.length,
         itemBuilder: (context, index) {
+          final product = products[index];
           return LSProductCard(
-            image: Assets.images.dataImage.path,
-            title: 'Nike Sportswear Club Fleece',
-            price: 123,
+            image: product.imageUrl,
+            title: product.name,
+            price: product.price.toInt(),
             onTapProduct: () {},
           );
         },
       ),
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, stack) => Center(child: Text('Error: $error')),
     );
   }
 }
--- laza/lib/presentations/pages/home/widget/list_view_brand.dart
+++ laza/lib/presentations/pages/home/widget/list_view_brand.dart
@@ -1,43 +1,48 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:laza/core/extensions/context_extensions.dart';
-import 'package:laza/core/gen_assets/assets.gen.dart';
 import 'package:laza/core/router/routes.dart';
 import 'package:laza/presentations/widgets/brand.dart';
+import 'package:laza/providers/brand_provider.dart';
 
-class ListViewBrand extends StatelessWidget {
+class ListViewBrand extends ConsumerWidget {
   const ListViewBrand({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        context.pushNamed(
-          AppRoutesName.brandViewAll.name,
-          extra: e,
-        );
-      },
-      child: SizedBox(
-        height: 50.h,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 4,
-          shrinkWrap: true,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: LSBrand(
-                brandName: 'Nike',
-                brandLogo: Assets.images.dataImage.path,
-              ),
-            );
-          },
+  Widget build(BuildContext context, WidgetRef ref) {
+    final brandAsyncValue = ref.watch(brandProvider);
+
+    return brandAsyncValue.when(
+      data: (brand) => InkWell(
+        onTap: () {
+          context.pushNamed(
+            AppRoutesName.brandViewAll.name,
+            extra: e,
+          );
+        },
+        child: SizedBox(
+          height: 50.h,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: brand.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final brands = brand[index];
+              return Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: LSBrand(brandName: brands.name, brandLogo: brands.image),
+              );
+            },
+          ),
         ),
       ),
+      error: (error, stack) => Center(child: Text('Error: $error')),
+      loading: () => const Center(child: CircularProgressIndicator()),
     );
   }
 }
