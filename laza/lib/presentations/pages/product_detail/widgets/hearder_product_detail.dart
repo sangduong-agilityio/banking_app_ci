1b6cfe6624eb5c275c5c6788452c45681c0d1878
Sang Duong <sang.duong@asnet.com.vn>
2024-07-24 10:20:52 +0700

    Implement process call API for detail screen

--- laza/lib/presentations/pages/product_detail/widgets/hearder_product_detail.dart
+++ laza/lib/presentations/pages/product_detail/widgets/hearder_product_detail.dart
@@ -1,43 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:laza/core/extensions/context_extensions.dart';
-import 'package:laza/core/gen_assets/assets.gen.dart';
 import 'package:laza/presentations/widgets/app_bar.dart';
 import 'package:laza/presentations/widgets/icons.dart';
 import 'package:laza/presentations/widgets/images.dart';
+import 'package:laza/providers/product_provider.dart';
 
-class HeaderProductDetail extends StatelessWidget {
+class HeaderProductDetail extends ConsumerWidget {
   const HeaderProductDetail({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        LSImage(
-          imageUrl: Assets.images.dataImage.path,
-          width: double.infinity,
-          height: 375.h,
-          fit: BoxFit.cover,
-          borderRadius: 0,
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 65,
-              right: 20,
-              left: 20,
-            ),
-            child: LSAppBar(
-              onTappedBackButton: () => context.pop(),
-              icon: LSIcons.icArrowLeft,
-              rightButtonIcon: LSIcons.icBag,
-              onTappedRightButton: () {},
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productsAsyncValue = ref.watch(productsProvider);
+    return productsAsyncValue.when(
+      data: (product) => Stack(
+        clipBehavior: Clip.none,
+        children: [
+          LSImage(
+            imageUrl: product.last.imageUrl,
+            width: double.infinity,
+            height: 375.h,
+            fit: BoxFit.cover,
+            borderRadius: 0,
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 65,
+                right: 20,
+                left: 20,
+              ),
+              child: LSAppBar(
+                onTappedBackButton: () => context.pop(),
+                icon: LSIcons.icArrowLeft,
+                rightButtonIcon: LSIcons.icBag,
+                onTappedRightButton: () {},
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      error: (error, stack) => Center(
+        child: Text('Error: $error'),
+      ),
     );
   }
 }
--- laza/lib/presentations/pages/product_detail/widgets/list_view_product_detail.dart
+++ laza/lib/presentations/pages/product_detail/widgets/list_view_product_detail.dart
@@ -1,37 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:laza/core/extensions/context_extensions.dart';
-import 'package:laza/core/gen_assets/assets.gen.dart';
 import 'package:laza/presentations/widgets/images.dart';
+import 'package:laza/providers/product_provider.dart';
 
-class ListViewProductDetail extends StatelessWidget {
+class ListViewProductDetail extends ConsumerWidget {
   const ListViewProductDetail({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: SizedBox(
-        height: 77.h,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 4,
-          shrinkWrap: true,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(
-                right: 15,
-              ),
-              child: LSImage(
-                width: 77.w,
-                height: 77.h,
-                borderRadius: 10,
-                imageUrl: Assets.images.dataImage.path,
-              ),
-            );
-          },
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productsAsyncValue = ref.watch(productsProvider);
+    return productsAsyncValue.when(
+      data: (products) => Padding(
+        padding: const EdgeInsets.all(20),
+        child: SizedBox(
+          height: 77.h,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: products.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final product = products[index];
+              return Padding(
+                padding: const EdgeInsets.only(
+                  right: 15,
+                ),
+                child: LSImage(
+                    width: 77.w,
+                    height: 77.h,
+                    borderRadius: 10,
+                    imageUrl: product.imageUrl),
+              );
+            },
+          ),
         ),
+      ),
+      // TODO(SangDuong): Apply shimmer loading and handle error
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      error: (error, stack) => Center(
+        child: Text('Error: $error'),
       ),
     );
   }
