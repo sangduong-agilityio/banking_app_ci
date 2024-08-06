cf4891d141c98801b82ae34c54a995f7f0c9b922
Sang Duong <sang.duong@asnet.com.vn>
2024-08-06 14:28:10 +0700

    Update list cart product

--- laza/lib/presentations/pages/cart/cart_product.dart
+++ laza/lib/presentations/pages/cart/cart_product.dart
@@ -3,11 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:laza/core/extensions/context_extensions.dart';
 import 'package:laza/core/l10n/l10n_generated/l10n.dart';
-import 'package:laza/core/router/routes.dart';
+import 'package:laza/presentations/pages/cart/widgets/list_view_product.dart';
 import 'package:laza/presentations/widgets/app_bar.dart';
 import 'package:laza/presentations/widgets/icons.dart';
-import 'package:laza/presentations/widgets/product_card.dart';
-import 'package:laza/presentations/widgets/snack_bar.dart';
 import 'package:laza/providers/cart_provider.dart';
 
 class CartProductPage extends ConsumerWidget {
@@ -37,41 +35,14 @@ class CartProductPage extends ConsumerWidget {
                   SizedBox(height: 20.h),
                   Center(
                     child: Text(
-                      S.current.letGetStarted,
+                      S.current.listCartProduct,
                       style: context.textTheme.displayLarge,
                     ),
                   ),
                   SizedBox(height: 20.h),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: cartProducts.length,
-                      itemBuilder: (context, index) {
-                        final product = cartProducts[index];
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10.0),
-                          child: Dismissible(
-                            key: Key(product.id.toString()),
-                            direction: DismissDirection.endToStart,
-                            onDismissed: (direction) {
-                              cartNotifier.removeProduct(product);
-                              LSSnackBar.buildErrorSnackbar(
-                                  context,
-                                  '${product.name}'
-                                  '${S.current.removeFormCart}');
-                            },
-                            child: LSProductCard(
-                              image: product.imageUrl,
-                              title: product.name,
-                              price: product.price,
-                              onTapProduct: () {
-                                context.pushNamed(
-                                    AppRoutesName.productDetailPage.name);
-                              },
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+                    child: ListCartProduct(
+                        cartProducts: cartProducts, cartNotifier: cartNotifier),
                   ),
                 ],
               ),
---
+++ laza/lib/presentations/pages/cart/widgets/card_product.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:laza/core/extensions/context_extensions.dart';
+import 'package:laza/presentations/widgets/icons.dart';
+import 'package:laza/presentations/widgets/images.dart';
+import 'package:laza/presentations/widgets/tag.dart';
+
+class LSCardProduct extends StatelessWidget {
+  final String image;
+  final String title;
+  final int price;
+  final void Function()? onTapProduct;
+
+  const LSCardProduct({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.onTapProduct,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTapProduct,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          LSImage(
+            imageUrl: image,
+            icon: LSIcons.icHeartBreak,
+            height: 170.h,
+            width: 140.w,
+          ),
+          const SizedBox(width: 15),
+          LSTag(
+            tagTitle: title,
+            tagPrice: price,
+          )
+        ],
+      ),
+    );
+  }
+}
