b6b63103561ae00d20da6498c1fc5fabd47e0488
Sang Duong <sang.duong@halliburton.com>
2025-03-28 10:54:25 +0700

    Set up folder laza application

---
+++ laza-app/lib/environments/env.dart
@@ -0,0 +1,15 @@
+// import 'package:envied/envied.dart';
+
+// part 'env.g.dart';
+
+// @Envied(path: './.env.dev')
+// abstract class Env {
+//   @EnviedField(varName: 'SUPABASE_URL')
+//   static const String supabaseUrl = _Env.supabaseUrl;
+
+//   @EnviedField(varName: 'SUPABASE_KEY')
+//   static const String supabaseKey = _Env.supabaseKey;
+
+//   @EnviedField(varName: 'SUPABASE_ENDPOINT')
+//   static const String endPoint = _Env.endPoint;
+// }
--- laza/android/app/src/main/kotlin/com/example/laza/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.laza
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
