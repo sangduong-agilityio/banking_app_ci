921af2e877a7b33806407aac5c302ae582bb9359
Sang Duong <sang.duong@halliburton.com>
2025-10-07 16:17:45 +0700

    remove print in api client

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -155,7 +155,6 @@ class BankingApiClient {
     try {
       final fingerprint = _getCertificateFingerprint(cert);
       print('CERT_DEBUG: Host: $host, Fingerprint: $fingerprint');
-      print('CERT_DEBUG: Subject: ${cert.subject}');
       print('CERT_DEBUG: Issuer: ${cert.issuer}');
       print('CERT_DEBUG: Valid from: ${cert.startValidity}');
       print('CERT_DEBUG: Valid to: ${cert.endValidity}');
