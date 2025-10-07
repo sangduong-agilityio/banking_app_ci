b333087793c0e00ab008e9dcace2c40ea4971660
Sang Duong <sang.duong@halliburton.com>
2025-10-07 16:19:58 +0700

    remove print in api client

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -157,7 +157,6 @@ class BankingApiClient {
       print('CERT_DEBUG: Host: $host, Fingerprint: $fingerprint');
       print('CERT_DEBUG: Issuer: ${cert.issuer}');
       print('CERT_DEBUG: Valid from: ${cert.startValidity}');
-      print('CERT_DEBUG: Valid to: ${cert.endValidity}');
     } catch (e) {
       print('CERT_DEBUG: Error logging certificate info: $e');
     }
